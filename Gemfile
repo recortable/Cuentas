163e3305e1b878ce230b4ec373bed622ba925926
Danigb <danigb@gmail.com>
2011-02-09 13:50:08 +0100

    rails3 project

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.4'
+
+gem 'omniauth'
+gem "simple_form"
+gem "cancan"
+
+gem 'capistrano'
+gem 'ruby-debug19'
+gem 'nokogiri'
+gem 'mysql2'
+
+# gem 'bj'
+# gem 'aws-s3', :require => 'aws/s3'
+
+group :development, :test do
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cuentas::Application.config.secret_token = '42813e1160bfda795dad348ce8f8350f9400a3a15b679cf3f160dbf49848eee0a24d132c91a3b69205651cfb7d1864d6d978bf610204c38e27dc0bbe5ea15c6b'
