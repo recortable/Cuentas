a240ac082854633bd5ee6c89fc781445d1f6a7b0
Danigb <danigb@gmail.com>
2011-02-15 15:47:09 +0100

    capify

---
+++ config/deploy.rb
@@ -0,0 +1,44 @@
+# SOURCES setup users: http://www.viget.com/extend/building-an-environment-from-scratch-with-capistrano-2/
+# setup deploy: http://www.capify.org/getting-started/from-the-beginning/
+
+# default_run_options[:pty] = true
+set :application, "Cuentas"
+set :deploy_to, "/home/deploy/#{application}"
+set :user, "deploy"
+set :use_sudo, false
+
+set :scm, "git"
+set :repository,  "git://github.com/recortable/Cuentas.git"
+set :branch, "master"
+set :deploy_via, :remote_cache
+set :scm_verbose, true
+
+role :app, "recortable.net"
+role :web, "recortable.net"
+role :db,  "recortable.net", :primary => true
+
+after "deploy:update_code", "config:copy_shared_configurations"
+after "deploy", "deploy:cleanup"
+
+# Configuration Tasks
+namespace :config do
+  desc "copy shared configurations to current"
+  task :copy_shared_configurations, :roles => [:app] do
+    %w[database.yml].each do |f|
+      run "ln -nsf #{shared_path}/config/#{f} #{release_path}/config/#{f}"
+    end
+  end
+end
+
+
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
