e52b0bf25b26e6315964a74347b2a45e64066a28
Danigb <danigb@gmail.com>
2011-03-01 12:39:08 +0100

    omniauth redirect

---
+++ config/initializers/omniauth_redirect.rb
@@ -0,0 +1,14 @@
+if Rails.env.production?
+  module OmniAuth
+    module Strategy
+      def full_host
+        uri = URI.parse(request.url)
+        uri.path = ''
+        uri.query = nil
+        uri.port = (uri.scheme == 'https' ? 443 : 80)
+        uri.to_s
+      end
+    end
+  end
+end
+
