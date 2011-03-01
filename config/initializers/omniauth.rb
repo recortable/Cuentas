cb55c6efdefa66e32fc9a4b915cd402527e85828
Danigb <danigb@gmail.com>
2011-03-01 12:47:57 +0100

    omniauth host

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,10 @@
 require 'openid/store/filesystem'
 
+OmniAuth.configure do |config|
+  config.full_host = 'http://cuentas.recortable.net'
+end
+
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :open_id, OpenID::Store::Filesystem.new('/tmp')
   provider :openid, nil, :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
